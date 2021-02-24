892e751c883399f37eb6ff041404aa15a30e088c
Luckyaremu <aremu.baba70@gmail.com>
2021-02-24 22:31:17 +0100

    ruby linkedlist coding challange

--- Ruby/2.linked_list.rb
+++ Ruby/2.linked_list.rb
@@ -32,6 +32,7 @@ class Node
       def remove(index)
         @list.delete_at(index)
       end
+      
       private
 
   def get_node(index)
@@ -41,3 +42,13 @@ end
 
 list = LinkedList.new
     
+list.add(3)
+list.add(5)
+list.add_at(1, 11)
+list.add_at(0, 13)
+
+puts list.get(2)
+# => 11
+
+puts list.get(3)
+# => 5
