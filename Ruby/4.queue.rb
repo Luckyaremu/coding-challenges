8dc8a958941abec22099aaf6f69913b78b16a180
Luckyaremu <aremu.baba70@gmail.com>
2021-02-27 17:29:39 +0100

    ruby queue coding challange

--- Ruby/4.queue.rb
+++ Ruby/4.queue.rb
@@ -57,3 +57,33 @@ class Stack
         def add(number)
           @queue.add_at(0, number)
         end
+        def remove
+            if @queue.get(0).nil?
+              -1
+            else
+              @queue.remove(-1)
+            end
+          end
+        end
+        
+        queue = Queue.new
+        
+        queue.add(3)
+        queue.add(5)
+        puts queue.remove
+        # => 3
+        
+        queue.add(2)
+        queue.add(7)
+        puts queue.remove
+        # => 5
+        
+        puts queue.remove
+        # => 2
+        
+        puts queue.remove
+        # => 7
+        
+        puts queue.remove
+        # => -1
+        
