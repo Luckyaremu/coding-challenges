617f00fd81af4f66c22ce417d318cf6a24a83a54
Luckyaremu <aremu.baba70@gmail.com>
2021-03-04 22:27:24 +0100

    sliding_maximum coding challenge added

--- Ruby/6.sliding_maximum.rb
+++ Ruby/6.sliding_maximum.rb
@@ -2,3 +2,10 @@ def sliding_maximum(key, array)
     i = 0
     bigs = []
     limit = array.length - key + 1
+    while i < limit
+        j = i
+        big = array[j]
+        while (j + 2) <= (key + i)
+          big = array[j + 1] if big <= array[j + 1]
+          j += 1
+        end
